6c10bb198155e57cdee1349324382a2ae6c29285
Trung Phan <pttrungg@gmail.com>
2021-10-17 14:51:15 -0500

    fix bug

--- app/src/main/java/miu/compro/cs743/myapplication/ui/fragments/search/SearchFragment.kt
+++ app/src/main/java/miu/compro/cs743/myapplication/ui/fragments/search/SearchFragment.kt
@@ -1,14 +1,19 @@
 package miu.compro.cs743.myapplication.ui.fragments.search
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.widget.SearchView
 import miu.compro.cs743.myapplication.R
 import miu.compro.cs743.myapplication.base.BaseFragment
 import miu.compro.cs743.myapplication.databinding.FragmentSearchBinding
+import miu.compro.cs743.myapplication.ui.activity.articledetail.ArticleDetailActivity
 import miu.compro.cs743.myapplication.ui.fragments.newslist.NewsAdapter
+import miu.compro.cs743.myapplication.ui.fragments.newslist.NewsListFragment
+import miu.compro.cs743.myapplication.ui.fragments.newslist.NewsListFragment.Companion.BOOKMARK_CLICKED
+import miu.compro.cs743.myapplication.ui.fragments.newslist.NewsListFragment.Companion.ITEM_CLICKED
+import miu.compro.cs743.myapplication.ui.fragments.newslist.NewsListFragment.Companion.SHARE_CLICKED
 import org.koin.android.viewmodel.ext.android.viewModel
-import android.provider.SearchRecentSuggestions
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate), SearchView.OnQueryTextListener {
     private val searchViewModel: SearchViewModel by viewModel()
@@ -51,13 +56,6 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding
 
     override fun onQueryTextSubmit(keyword: String?): Boolean {
         searchViewModel.searchArticleByKeyword(keyword)
-
-        val suggestions = SearchRecentSuggestions(
-            requireContext(),
-            MySuggestionProvider.AUTHORITY,
-            MySuggestionProvider.MODE
-        )
-        suggestions.saveRecentQuery(keyword, null)
         return false
     }
 
@@ -68,7 +66,25 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding
     private fun observeData() {
         searchViewModel.article.observe(viewLifecycleOwner, { articles ->
             articles?.let {
-                binding.rvArticles.adapter = NewsAdapter(articles)
+                binding.rvArticles.adapter = NewsAdapter(articles).apply {
+                    setOnItemClickListener { which, position, article, rootView ->
+                        when (which) {
+                            ITEM_CLICKED -> {
+                                val intent = Intent(requireActivity(), ArticleDetailActivity::class.java).apply {
+                                    putExtra("article", article)
+                                }
+                                startActivity(intent)
+
+                            }
+                            BOOKMARK_CLICKED -> {
+
+                            }
+                            SHARE_CLICKED -> {
+
+                            }
+                        }
+                    }
+                }
             }
         })
     }
